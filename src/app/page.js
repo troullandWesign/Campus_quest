1f58dfa7fc13f2c983a6c63bffb346a3042b9b3c
Thomas ROULLAND <troulland@wesign.fr>
2024-01-12 16:46:26 +0100

    build

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
 "use client";
 import Image from 'next/image'
-import { useRouter } from 'next/navigation';
-
 
 export default function Home() {
-  const router = useRouter();
-  const isAuthenticated = true; // Définissez cela sur le statut d'authentification réel
   return (
     <main>
       <div>
@@ -14,15 +10,14 @@ export default function Home() {
          className="h-screen w-full bg-gray-900 bg-cover bg-no-repeat"
          style={{
            backgroundImage:
-             'url("https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80")'
+          "url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')"
          }}
          >
-          <h1>Bienvenue sur la page d'accueil !</h1>
+          <h1>Bienvenue sur la page d&apos;accueil !</h1>
           <p>Vous êtes maintenant authentifié.</p>
         </div>
       ) : (
-        // Rendre un autre contenu pour les utilisateurs non authentifiés si nécessaire
-        <h1>Bienvenue sur la page d'accueil !</h1>
+        <h1>Bienvenue sur la page d&apos;accueil !</h1>
       )}
       </div>
     </main>
