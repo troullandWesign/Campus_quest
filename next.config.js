83cea17ca6be2e33edd8bb2e6298fbc56461c17c
Thomas ROULLAND <troulland@wesign.fr>
2024-01-12 12:37:02 +0100

    update manifest

--- next.config.js
+++ next.config.js
@@ -2,3 +2,20 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+      disableDevLogs: true,
+    },
+    // ... other options you like
+  });
+
+  
+  module.exports = withPWA(nextConfig);
+
